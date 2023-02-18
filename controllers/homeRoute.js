1cfbae8fffaf69d11b0b91a3d044314cfbf58f2e
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-17 20:13:12 -0600

    Cleaned up code in homeroutes.

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -9,20 +9,18 @@ const today = moment().format("YYYY-MM-DD");
 const tomorrow = moment().add(1, "days").format("YYYY-MM-DD");
 const weekAgo = moment().subtract(7, "days").format("YYYY-MM-DD HH:MM:SS");
 const Sequelize = require('sequelize')
-// destroys asteroids older than 14 hours in the database
 
 router.get("/", async (req, res) => {
+
+  // This destroys asteroids older than 14 hours in the database
    await Asteroid.destroy({ where: {
      createdAt: {
       [Sequelize.lte]: new Date(Date.now() - (60*60*14*1000))
     }
    }});
 
-   // pulls in asteroid data from NASA
-
-  const nasaData = await axios.get(
-    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&api_key=m3HKKEeMd83xzbasILLhUhnjvaYnkqmbJVmfOMuU`
-  );
+   // This pulls in asteroid data from NASA
+  const nasaData = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&api_key=m3HKKEeMd83xzbasILLhUhnjvaYnkqmbJVmfOMuU`);
 
   const data = await nasaData.data.near_earth_objects;
   let promises = [];
@@ -34,26 +32,20 @@ router.get("/", async (req, res) => {
       const awaitme = await promises.push({
           name: correctedName,
           diameter: element.estimated_diameter.feet.estimated_diameter_max,
-          speed:
-            element.close_approach_data[0].relative_velocity.miles_per_hour,
+          speed: element.close_approach_data[0].relative_velocity.miles_per_hour,
           hazardous: element.is_potentially_hazardous_asteroid,
           close_date: element.close_approach_data[0].close_approach_date_full,
           miss_distance: element.close_approach_data[0].miss_distance.lunar,
-
-    
         });
-   
     });
   }
 
   //This line bulk creates asteroids, and removes all duplicates.
-
   Asteroid.bulkCreate(promises, {ignoreDuplicates: true})
 
   const results = await Asteroid.findAll({});
 
 // This sends the asteroids to the home page and renders them.
-
   const asteroids = results.map((roidz) => roidz.get({ plain: true }));
  
   res.render("homepage", {
@@ -63,7 +55,6 @@ router.get("/", async (req, res) => {
 });
 
 //if logged in it will present the single asteroid route with subsequent comments, and the associated users.
-
 router.get("/asteroid/:id", authenticated, (req, res) => {
   Asteroid.findOne({
     where: {
@@ -87,7 +78,6 @@ router.get("/asteroid/:id", authenticated, (req, res) => {
 });
 
 // If user is logged in it redirects to home page else redirect to login page.
-
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
@@ -97,7 +87,6 @@ router.get("/login", (req, res) => {
 });
 
 // Takes user to signup route.
-
 router.get("/signup", (req, res) => {
   res.render("signup");
 });
